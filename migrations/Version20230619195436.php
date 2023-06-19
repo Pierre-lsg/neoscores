4d1e6129ef09bd0af0e777b5301ce13eb2f6f577
Pierre Bonnafous <pierre.bonnafous@gmail.com>
2023-06-19 21:55:47 +0200

    Adding Chmapionship templates and Competition entity

---
+++ migrations/Version20230619195436.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230619195436 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE competition (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, order_c INT NOT NULL, date_start_competition DATE NOT NULL, date_scores_publishing DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE competition');
+    }
+}
---
+++ src/Entity/Competition.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompetitionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CompetitionRepository::class)]
+class Competition
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $orderC = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateStartCompetition = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateScoresPublishing = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOrderC(): ?int
+    {
+        return $this->orderC;
+    }
+
+    public function setOrderC(int $orderC): static
+    {
+        $this->orderC = $orderC;
+
+        return $this;
+    }
+
+    public function getDateStartCompetition(): ?\DateTimeInterface
+    {
+        return $this->dateStartCompetition;
+    }
+
+    public function setDateStartCompetition(\DateTimeInterface $dateStartCompetition): static
+    {
+        $this->dateStartCompetition = $dateStartCompetition;
+
+        return $this;
+    }
+
+    public function getDateScoresPublishing(): ?\DateTimeInterface
+    {
+        return $this->dateScoresPublishing;
+    }
+
+    public function setDateScoresPublishing(\DateTimeInterface $dateScoresPublishing): static
+    {
+        $this->dateScoresPublishing = $dateScoresPublishing;
+
+        return $this;
+    }
+}
