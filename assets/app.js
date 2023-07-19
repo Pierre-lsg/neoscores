9690d1e6d437cb1a9c47e617ae827fa38c822403
Pierre Bonnafous <pierre.bonnafous@gmail.com>
2023-07-19 22:30:44 +0200

    Some changes

--- assets/app.js
+++ assets/app.js
@@ -14,3 +14,18 @@ import bsCustomFileInput from 'bs-custom-file-input';
 // start the Stimulus application
 import './bootstrap';
 bsCustomFileInput.init();
+
+
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
